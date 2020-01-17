7cce9875c00783faa8619d06d572b19b770a1f26
陶辰 <chen.tao@ifchange.com>
2020-01-17 15:52:37 +0800

    update

--- examples/tls.go
+++ examples/tls.go
@@ -9,14 +9,14 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handlerTls(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("WORLD!"))
 }
 
 func main() {
 	mux1 := mux.NewRouter()
 	mux1.Schemes("https")
-	mux1.HandleFunc("/hello", handler).
+	mux1.HandleFunc("/hello", handlerTls).
 		Methods("GET")
 
 	err := endless.ListenAndServeTLS("localhost:4242", "cert.pem", "key.pem", mux1)
